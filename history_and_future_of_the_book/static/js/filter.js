d1a652eef7d7f9cbc58f0d48f67cd2a27d67f0bd
hayley <yuehu2018@u.northwestern.edu>
2015-07-15 10:17:17 -0500

    create superuser

--- history_and_future_of_the_book/static/js/filter.js
+++ history_and_future_of_the_book/static/js/filter.js
@@ -1,46 +1,15 @@
 $(document).ready(function() {
-    var activeSystemClass = $('.list-group-item.active');
-
-    //something is entered in search form
-    $('#system-search').keyup( function() {
-       var that = this;
-        // affect all table rows on in systems table
-        var tableBody = $('.table-list-search tbody');
-        var tableRowsClass = $('.table-list-search tbody tr');
-        $('.search-sf').remove();
-        tableRowsClass.each( function(i, val) {
-        
-            //Lower text for case insensitive
-            var rowText = $(val).text().toLowerCase();
-            var inputText = $(that).val().toLowerCase();
-            if(inputText != '')
-            {
-                $('.search-query-sf').remove();
-                tableBody.prepend('<tr class="search-query-sf"><td colspan="6"><strong>Searching for: "'
-                    + $(that).val()
-                    + '"</strong></td></tr>');
-            }
-            else
-            {
-                $('.search-query-sf').remove();
-            }
-
-            if( rowText.indexOf( inputText ) == -1 )
-            {
-                //hide rows
-                tableRowsClass.eq(i).hide();
-                
-            }
-            else
-            {
-                $('.search-sf').remove();
-                tableRowsClass.eq(i).show();
-            }
-        });
-        //all tr elements are hidden
-        if(tableRowsClass.children(':visible').length == 0)
-        {
-            tableBody.append('<tr class="search-sf"><td class="text-muted" colspan="6">No entries found.</td></tr>');
-        }
-    });
+    var myConnect = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=d:\\sdi.mdb"; 
+ 
+    var ConnectObj = new ActiveXObject("ADODB.Connection");
+    var RS = new ActiveXObject("ADODB.Recordset");
+    var sql="SELECT * FROM history_and_future_of_the_book_table WHERE file_position='A07686+dwdID1';";
+ 
+    ConnectObj.Open (myConnect);
+    RS.Open(sql,ConnectObj,adOpenForwardOnly,adLockReadOnly,adCmdText);
+ 
+    var fieldCount = RS.Fields.Count;
+    alert("Field Count" + fieldCount);    
+    RS.Close();
+    ConnectObj.Close();
 });
